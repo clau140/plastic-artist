4f8b5755b7e986417fa59eb0dfcd5f1df9bb44e2
clau140 <clau.alberdi89@gmail.com>
2024-07-24 00:07:47 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+const PORT= process.env.DB_PORT || 3001;
+//const PORT= process.env.DB_PORT
+
+
+// Syncing all the models at once.
+conn.sync({ force: false }).then(() => {
+server.listen(PORT, async () => {
+  
+  console.log(`Server listening on port ${PORT}`);
+})
+}).catch(error => console.error(error))
