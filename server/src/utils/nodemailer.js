2159f01fa6510aa1ce9b2ad47a84fc482e6158a5
clau140 <clau.alberdi89@gmail.com>
2024-08-03 00:39:56 -0300

    procesing nodemailer

---
+++ server/src/utils/nodemailer.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer');
+
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail', // Usa el servicio de correo que prefieras
+  auth: {
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASS, 
+  },
+});
+
+const sendMail = (to, subject, text) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  };
+
+  return transporter.sendMail(mailOptions);
+};
+
+module.exports = sendMail;
