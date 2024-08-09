10ba3c1447b31ce679158f3b286d8c8713d6a5aa
clau140 <clau.alberdi89@gmail.com>
2024-08-09 01:32:50 -0300

    transition

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
-
 import Contact from './pages/Contact';
 import Jobs from './components/Jobs';
 import Dashboard from './pages/Dashboard';
@@ -10,19 +10,35 @@ import UpdateJob from './components/UpdateJob';
 import WorkDetail from './components/WorkDetail';
 import './index.css';
 
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        key={location.pathname}
+        timeout={300}
+        classNames="fade"
+      >
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/jobs/:id" element={<WorkDetail />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/jobs/edit/:id" element={<UpdateJob />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <main className="pt-16"> 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route exact path="/jobs/:id" element={<WorkDetail />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/jobs/edit/:id" element={<UpdateJob />} />
-      </Routes>
+      <main className="pt-16">
+        <AnimatedRoutes />
       </main>
     </Router>
   );
