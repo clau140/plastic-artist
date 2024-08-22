008f78e04b03e7665549d7fd05d353a5ca57cf7f
clau140 <clau.alberdi89@gmail.com>
2024-08-22 05:53:03 -0300

    detaile styles

--- client/src/components/WorkDetail.jsx
+++ client/src/components/WorkDetail.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { fetchJobById } from '../redux/slices/jobsSlice';
+import { FaTimes } from 'react-icons/fa';
 
 const WorkDetail = () => {
   const { id } = useParams();
@@ -26,10 +27,26 @@ const WorkDetail = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">{selectedJob.title}</h1>
-      <img src={selectedJob.image} alt={selectedJob.title} className="w-full h-80 object-cover mb-4" />
-      <p className="text-gray-600 mb-4">{selectedJob.description}</p>
-      <p className="text-gray-800 font-bold">{selectedJob.category}</p>
+      <div className="mb-8 mt-12">
+        <Link to="/" className="text-sky-blue hover:underline">
+          Volver a Inicio
+        </Link>
+      </div>
+      
+      <div className="relative bg-white shadow-lg rounded-2xl p-4">
+        <Link to="/jobs" className="absolute top-2 right-2 text-gray-500 hover:text-gray-900 transition-colors duration-200">
+          <FaTimes size={20} />
+        </Link>
+
+        <h1 className="text-3xl font-bold mb-4">{selectedJob.title}</h1>
+        <img
+          src={selectedJob.image}
+          alt={selectedJob.title}
+          className="w-full h-80 object-cover mb-4 rounded-xl"
+        />
+        <p className="text-gray-600 mb-4">{selectedJob.description}</p>
+        <p className="text-gray-800 font-bold">{selectedJob.category}</p>
+      </div>
     </div>
   );
 };
