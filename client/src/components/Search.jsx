a8f0d11d644af8c078eb564f2d1be59880c05dbe
clau140 <clau.alberdi89@gmail.com>
2024-08-22 03:43:31 -0300

    search update

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -19,14 +19,14 @@ const Search = () => {
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
-            viewBox="0 0 20 20"
+            viewBox="0 0 21 21"
           >
             <path
               stroke="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              d="M11.15 5.6h.01m3.337 1.913h.01m-6.979 0h.01M5.541 11h.01M15 15h2.706a1.957 1.957 0 0 0 1.883-1.325A9 9 0 1 0 2.043 11.89 9.1 9.1 0 0 0 7.2 19.1a8.62 8.62 0 0 0 3.769.9A2.013 2.013 0 0 0 13 18v-.857A2.034 2.034 0 0 1 15 15Z"
             />
           </svg>
         </div>
@@ -34,7 +34,7 @@ const Search = () => {
           type="text"
           value={searchQuery}
           onChange={handleSearchChange}
-          placeholder="Search Mockups, Logos, Design Templates..."
+          placeholder="Busca un trabajo..."
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
       </div>
@@ -43,5 +43,6 @@ const Search = () => {
 };
 
 export default Search;
+
 
 
