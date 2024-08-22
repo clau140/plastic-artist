de21fbeae45930d2d0f56145bf2fcb8c63c276e7
clau140 <clau.alberdi89@gmail.com>
2024-08-22 03:42:30 -0300

    search

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setSearchQuery } from '../redux/slices/jobsSlice'; 
+import { setSearchQuery } from '../redux/slices/jobsSlice';
 
 const Search = () => {
   const dispatch = useDispatch();
@@ -11,16 +11,37 @@ const Search = () => {
   };
 
   return (
-    <div className="relative">
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={handleSearchChange}
-        placeholder="Buscar trabajos"
-        className="px-4 py-2 rounded-md border border-gray-300 bg-white shadow-sm focus:outline-none focus:ring-2 focus:ring-sky-blue"
-      />
-    </div>
+    <form className="flex items-center max-w-lg mx-auto">
+      <div className="relative w-full">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          placeholder="Search Mockups, Logos, Design Templates..."
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+    </form>
   );
 };
 
 export default Search;
+
+
