f3cc7e1c4639f3d885c8225f1a12e4212a5255e4
clau140 <clau.alberdi89@gmail.com>
2024-08-22 05:15:26 -0300

    services styles

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import pinturaImage from '../assets/pintura.png'; 
 
 const Services = () => {
   return (
@@ -9,49 +10,49 @@ const Services = () => {
           Volver a Inicio
         </Link>
       </div>
-      <div className="relative container mx-auto p-6 rounded-lg shadow-2xl ">
+      <div className="relative container mx-auto p-6 rounded-lg shadow-2xl">
        
-        <div className="absolute inset-0 rounded-lg  bg-black opacity-60"></div>
+        <div className="absolute inset-0 rounded-lg bg-black opacity-60"></div>
 
         <div className="relative bg-transparent p-6 rounded-lg">
           <h1 className="text-3xl font-bold mb-4">Servicios Ofrecidos</h1>
-          <ul className="list-inside">
-            <li className="mb-4 flex items-start">
+          <ul className="list-inside grid grid-cols-1 md:grid-cols-2 gap-6 mb-9 mt-9"> 
+            <li className="flex items-start">
               <span className="w-4 h-4 mr-4 text-blue-500">•</span>
               <div>
                 <h2 className="text-xl font-semibold">Cuadros Personalizados</h2>
                 <p className="text-gray-700">Creación de cuadros únicos según las preferencias del cliente.</p>
               </div>
             </li>
-            <li className="mb-4 flex items-start">
+            <li className="flex items-start">
               <span className="w-4 h-4 mr-4 text-blue-500">•</span>
               <div>
                 <h2 className="text-xl font-semibold">Retratos</h2>
                 <p className="text-gray-700">Retratos detallados y personalizados para capturar momentos especiales.</p>
               </div>
             </li>
-            <li className="mb-4 flex items-start">
+            <li className="flex items-start">
               <span className="w-4 h-4 mr-4 text-blue-500">•</span>
               <div>
                 <h2 className="text-xl font-semibold">Carteles Personalizados</h2>
                 <p className="text-gray-700">Diseño de carteles a medida para eventos, negocios y más.</p>
               </div>
             </li>
-            <li className="mb-4 flex items-start">
+            <li className="flex items-start">
               <span className="w-4 h-4 mr-4 text-blue-500">•</span>
               <div>
                 <h2 className="text-xl font-semibold">Murales</h2>
                 <p className="text-gray-700">Murales artísticos para decorar espacios públicos y privados.</p>
               </div>
             </li>
-            <li className="mb-4 flex items-start">
+            <li className="flex items-start">
               <span className="w-4 h-4 mr-4 text-blue-500">•</span>
               <div>
                 <h2 className="text-xl font-semibold">Clases particulares</h2>
                 <p className="text-gray-700">Clases particulares para alumnos de secundaria.</p>
               </div>
             </li>
-            <li className="mb-4 flex items-start">
+            <li className="flex items-start">
               <span className="w-4 h-4 mr-4 text-blue-500">•</span>
               <div>
                 <h2 className="text-xl font-semibold">Otros</h2>
@@ -73,7 +74,13 @@ const Services = () => {
               Contáctame
             </Link>
           </div>
+         
         </div>
+        <img
+            src={pinturaImage}
+            alt="mancha de pintura"
+            className="absolute top-1 right-3 w-32 h-auto"
+          />
       </div>
     </div>
   );
