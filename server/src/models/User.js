2c8bf368bee5df83b3e57ff587e368034daa33b4
clau140 <clau.alberdi89@gmail.com>
2024-08-23 04:59:23 -0300

    add model User

---
+++ server/src/models/User.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const User = sequelize.define('user', {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  
+    return User;
+  };
+  
