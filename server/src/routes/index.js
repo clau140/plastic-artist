881a90f05d88ef70e97f418607020d7f817de625
clau140 <clau.alberdi89@gmail.com>
2024-08-30 12:02:02 -0300

    user add bcrypt

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Router } = require('express');
 
-const professorRouter = require('./professor')
+
 const jobsRouter = require('./jobs');
 const contactRouter = require('./contact');
 
@@ -9,7 +9,7 @@ const router = Router();
 
 
 router.use(express.json());
-router.use('/professor', professorRouter);
+
 router.use('/jobs', jobsRouter);
 router.use('/contact', contactRouter);
 
