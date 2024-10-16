a3357dce299d262d0c6a546d7e06415566d81b34
clau140 <clau.alberdi89@gmail.com>
2024-10-16 01:01:26 -0300

    style card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,14 +6,16 @@ const Card = ({ job }) => {
     <div className="rounded-lg shadow-lg transition-shadow duration-300 ease-in-out overflow-hidden transform hover:scale-105">
       <Link to={`/jobs/${job.id}`}>
         <img
-          src={job.images && job.images.length > 0 ? job.images[0] : 'default-image-url'} // Reemplaza 'default-image-url' por una URL por defecto si no hay imágenes
+          src={job.images && job.images.length > 0 ? job.images[0] : 'default-image-url'} 
           alt={job.title}
           className="w-full h-48 object-cover"
         />
         <div className="p-6 bg-white">
           <h2 className="text-2xl font-semibold mb-3 text-gray-800 hover:text-blue-600 transition-colors duration-300 ease-in-out">{job.title}</h2>
+          <span className="bg-blue-500 text-white text-xs font-bold px-2 py-1 rounded-full inline-block mb-3">
+            {job.category}
+          </span>
           <p className="text-gray-600 mb-3">{job.description}</p>
-          <p className="text-gray-800 font-bold">{job.category}</p>
         </div>
       </Link>
     </div>
